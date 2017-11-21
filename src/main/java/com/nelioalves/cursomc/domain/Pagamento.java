af6e3a9d1dff1a0e2f353cc26f41ac81f8db5e84
Nelson Florencio Junior <nflorencio@stefanini.com>
2017-11-21 11:16:43 -0200

    AULA 21 - Pedido, EstadoPagamento e Pagamento - Parte 2(OneToOne)

--- src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
@@ -2,14 +2,25 @@ package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
 import com.nelioalves.cursomc.domain.enuns.EstadoPagamento;
 
+@Entity
 public class Pagamento implements Serializable { //Serializable.: Objetos podem ser convertidos em arquivos para trafegar em rede e etc.(padrão/exigência Java)
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Integer id;
 	private EstadoPagamento estado;
 	
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {
--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -3,12 +3,23 @@ package com.nelioalves.cursomc.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Pedido implements Serializable { //Serializable.: Objetos podem ser convertidos em arquivos para trafegar em rede e etc.(padrão/exigência Java)
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //definindo a estratégia de geração automatica de id's da tabela
 	private Integer id;
 	private Date instante;
 	
+	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
 	private Cliente cliente;
